43523fa0304d04ae7bf46923752ed26524be7e4e
Brunosccp <rocca_bruno@hotmail.com>
2018-08-24 15:55:40 -0300

    segundo commit

---
+++ Projeto Taipas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Projeto Taipas
+//
+//  Created by Bruno Rocca on 24/08/2018.
+//  Copyright © 2018 Bruno Rocca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
