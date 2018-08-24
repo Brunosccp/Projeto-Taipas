eeccb90304fff10261e059259c8f38d44092420c
stefanv.demoraes <stefanvargas3@gmail.com>
2018-08-24 16:50:52 -0300

    raw TableView done

---
+++ Projeto Taipas/TableView/FavTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FavTableViewCell.swift
+//  Projeto Taipas
+//
+//  Created by Stefan V. de Moraes on 24/08/2018.
+//  Copyright © 2018 Bruno Rocca. All rights reserved.
+//
+
+import UIKit
+
+class FavTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
