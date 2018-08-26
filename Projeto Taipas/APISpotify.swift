f5da5e6490141305b2d0d76799f93b38414edc7f
Brunosccp <rocca_bruno@hotmail.com>
2018-08-26 01:49:39 -0300

    adicionando swiftyJSON e função que cria url de request do spotify

--- Projeto Taipas/APISpotify.swift
+++ Projeto Taipas/APISpotify.swift
@@ -8,15 +8,20 @@
 
 import Foundation
 import Alamofire
-
+import SwiftyJSON
 
 class APISpotify{
     
     typealias JSONStandard = [String : AnyObject]
     
+    //constants that we got from spotify api project to get access token to make requests
     private let clientID = "dd6fc05e17654f07933f07d8602d7845"
     private let clientSecret = "da1e132fa2644d498fc75a0827c72e8a"
     var token: String?
+    
+    var urlRequest: String?
+    
+    
     
     func getAccessToken(completion: @escaping () -> ()){
             //parameters of spotify project to get the token
@@ -32,36 +37,42 @@ class APISpotify{
             })
         
     }
-    func callAlamo(url: String, completion: @escaping () -> ()){
+    func getURL(artist: String, type: String, limit: Int, offset: Int){
+        var artistDecoded = ""
+        for char in artist{
+            if (char != " "){
+                artistDecoded.append(char)
+            }
+            else{
+                artistDecoded += "%20"
+            }
+            //print("char: ",char)
+        }
+        print(artistDecoded)
+        
+        let URL = "https://api.spotify.com/v1/search?q=" + artistDecoded + "&type=" + type + "&market=BR" + "&limit=\(limit)" + "&offset=\(offset)"
+        
+        
+        print(URL)
+        urlRequest = URL
+        
+    }
+    func callAlamo(completion: @escaping (JSON) -> ()){
         let headers: HTTPHeaders = ["Authorization" : "Bearer " + token!]
         
-        Alamofire.request(url, headers: headers).responseJSON(completionHandler: {
+        Alamofire.request(urlRequest!, headers: headers).responseData(completionHandler: {
             response in
-            //let bob = response.data as! [String : Any]
-            self.parseData(JSONData: response.data!)
-            //print(bob)
+            if let data = response.result.value{
+                do{
+                    let jsonResult = try JSON(data: data)
+                    completion(jsonResult)
+                    
+                }catch{
+                    print(error)
+                }
+            }
             
         })
-    }
-    func parseData(JSONData: Data){
-        
-        //let item
-        
-    
-    }
-                
-                
-            
-            
-            
-            
-            
-            
-        }catch{
-            print(error)
-        }
-        
-        
     }
     
 }
--- Projeto Taipas/ViewController.swift
+++ Projeto Taipas/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Alamofire
+import SwiftyJSON
 
 class TableViewController: UITableViewController {
 
@@ -24,9 +25,16 @@ class TableViewController: UITableViewController {
         apiSpotify.getAccessToken(completion: {
             print(apiSpotify.token!)
             
+            apiSpotify.getURL(artist: "camarote", type: "track", limit: 20, offset: 10)
+            
             //calling closure to get info of URL
-            apiSpotify.callAlamo(url: "https://api.spotify.com/v1/search?q=Shawn%20Mendes&type=track&limit=20&offset=10", completion: {
+            apiSpotify.callAlamo(completion: {result in //result is the JSON
                 
+                for i in 0..<result["tracks"]["items"].count{
+                    print(result["tracks"]["items"][i]["name"])
+                    print(result["tracks"]["items"][i]["artists"][0]["name"])
+                    print()
+                }
                 
             })
         })
